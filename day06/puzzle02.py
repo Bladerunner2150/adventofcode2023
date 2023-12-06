e45cd8f38d6de5af8c93ee88051535a9879cb76e
Bart Taelemans <bart@taelemans>
2023-12-06 19:21:53 +0100

    Day 6

---
+++ day06/puzzle02.py
@@ -0,0 +1,36 @@
+from functools import reduce
+import operator
+
+times = []
+distances = []
+ways_to_win = [] # For each race, calculate the number of ways to win
+
+def fill_arrays(line):
+    if line.startswith("Time:"):
+        times.append(int(line.split(":")[1].replace(" ", "")))
+    elif line.startswith("Distance:"):
+        distances.append(int(line.split(":")[1].replace(" ", "")))
+
+def calculate_ways_to_win_for_race(index):
+    time = times[index]
+    distance_to_reach = distances[index] + 1
+    print(f"Calculating race {i + 1} with distance to reach: {distance_to_reach}")
+    can_finish = [] # Array of booleans per millisecond
+    possible_speed = 1
+    for holding_milli_second in range(1, time): # Ignore 0 milliseconds and the last millisecond
+        possible_distance = possible_speed * (time - holding_milli_second)
+        if possible_distance >= distance_to_reach:
+            can_finish.append(True)
+        else:
+            can_finish.append(False)
+        possible_speed += 1
+    ways_to_win.append(sum(can_finish))
+
+with open("input.txt", "r") as file:
+    for line in file:
+        fill_arrays(line.strip())
+
+for i in range(len(times)):
+    calculate_ways_to_win_for_race(i)
+
+print(reduce(operator.mul, ways_to_win))
